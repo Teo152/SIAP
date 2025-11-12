e6de010e7182c6b667cfabd51f3344dbca7c478f
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-11-11 19:40:22 -0500

    Conexion

--- lib_repositorios/Implementaciones/Conexion.cs
+++ lib_repositorios/Implementaciones/Conexion.cs
@@ -8,25 +8,26 @@ namespace lib_repositorios.Implementaciones
     {
         public string? StringConexion { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
+        public Conexion(DbContextOptions<Conexion> options) : base(options)
         {
-            optionsBuilder.UseSqlServer(this.StringConexion!, p => { });
-            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured && !string.IsNullOrEmpty(StringConexion))
+            {
+                optionsBuilder.UseSqlServer(StringConexion);
+            }
+
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
 
         public DbSet<Busquedas>? Busquedas { get; set; }
-
-      //  public DbSet<Estados>? Estados { get; set; }  Presguntar por este
-       public DbSet<Mensajes>? Mensajes { get; set; }
-
+        public DbSet<Mensajes>? Mensajes { get; set; }
         public DbSet<Propiedades>? Propiedades { get; set; }
-
         public DbSet<Pagos>? Pagos { get; set; }
         public DbSet<Resenas>? Resenas { get; set; }
         public DbSet<Reservas>? Reservas { get; set; }
         public DbSet<Usuarios>? Usuarios { get; set; }
-
     }
 }
